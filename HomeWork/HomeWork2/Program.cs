dd9d201257d89e11776447c470a9d51082143602
Bellamy <Spa1k199991@gmail.com>
2023-05-28 18:02:15 +0300

    V2

---
+++ HomeWork/HomeWork2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и
+//  выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int number3 = int.Parse(Console.ReadLine());
+
+if ((number1 > number2) & (number1 > number3))
+      Console.WriteLine($"{number1}, {number2}, {number3} -> {number1} ");
+else if (number2 > number3)
+    Console.WriteLine($"{number1}, {number2}, {number3} -> {number2} ");
+else 
+    Console.WriteLine($"{number1}, {number2}, {number3} -> {number3} ");
---
+++ HomeWork/HomeWork4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число
+//  (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// Формулу взял с интернета :) В математике не силен)
+
+Console.WriteLine("Введите число от 1 ");
+int number = int.Parse(Console.ReadLine());
+string s = "";
+
+while (number < 1)
+{
+    Console.Write("Вы ошиблись!Введите число от 1: ");
+    number = int.Parse(Console.ReadLine());
+}
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0) 
+        s = s + ($"{i } ");
+}
+
+Console.WriteLine($"{number} -> {s} ");
